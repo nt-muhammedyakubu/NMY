3e32863a86bdc995dc0da05ad91ebb54ecf4e880
Naimah Muhammed-Yakubu <naimah.muhammed-yakubu@uwe.ac.uk>
2021-03-20 05:16:11 +0000

    Delete Project4:Evaluate-news-nlp directory

--- Project4:Evaluate-news-nlp/src/tests/nameChecker.test.js
+++
@@ -1,8 +0,0 @@
-import { checkForName } from '../client/js/nameChecker';
-
-describe('Testing the names functionality', () => {
-    test('Name for testing', () => {
-        expect(checkForName('Archer')) === true;
-        expect(checkForName('Archier')) === false;
-    });
-})
