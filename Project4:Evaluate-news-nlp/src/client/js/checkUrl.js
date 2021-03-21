da36f3c36aa17d9644640aa1813b25524c17c427
Naimah Muhammed-Yakubu <naimah.muhammed-yakubu@uwe.ac.uk>
2021-03-21 12:42:59 +0000

    Delete Project4:Evaluate-news-nlp directory

--- Project4:Evaluate-news-nlp/src/client/js/checkUrl.js
+++
@@ -1,9 +0,0 @@
-export function checkUrl(url) {
-    const regexr = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g;
-    if (regexr.test(url)) {
-        return true;
-
-    } else {
-        return false;
-    }
-}
--- Project4:Evaluate-news-nlp/src/tests/formHandler.test.js
+++
@@ -1,7 +0,0 @@
-import { submitHandler } from '..client/js/formHandler';
-
-describe('Testing the form submit functionality', () => {
-    test('submit form testing', () => {
-        expect(submitHandler).toBeDefined();
-    });
-})
